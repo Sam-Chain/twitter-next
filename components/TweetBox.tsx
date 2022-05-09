045e1c8eb76af6d72bbd86258cbd69ee2a5d4bf6
sam-chain <merah.saddam@gmail.com>
2022-05-09 05:14:09 +0100

    tweet box done!

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -1,20 +1,68 @@
+import {
+  CalendarIcon,
+  EmojiHappyIcon,
+  LocationMarkerIcon,
+  PhotographIcon,
+  SearchCircleIcon,
+} from '@heroicons/react/outline'
 import Image from 'next/image'
-import React from 'react'
+import { useState } from 'react'
 
 const TweetBox = () => {
+  const [input, setInput] = useState<string>('')
   return (
-    <div className="relative">
-      <Image
-        className="h-14 w-14 rounded-full object-cover"
-        alt=""
-        src="https://links.papareact.com/gll"
-        // objectFit="cover"
-        // width={14}
-        // height={14}
-        // layout="responsive"
-        // or
-        layout="fill"
-      />
+    <div className="flex space-x-2 p-5">
+      <div className="relative h-14 w-14">
+        <Image
+          alt=""
+          src="https://links.papareact.com/gll"
+          className="rounded-full"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <div className="flex flex-1 items-center pl-2 ">
+        <form className="flex flex-1 flex-col">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="h-24 w-full text-xl outline-none placeholder:text-xl "
+            type="text"
+            placeholder="What's Happening?"
+          />
+          <div className="flex items-center ">
+            <div className="flex flex-1  space-x-2 text-twitter ">
+              <PhotographIcon
+                className="h-5 w-5 cursor-pointer transition-transform duration-150
+              ease-out hover:scale-150 "
+              />
+              <SearchCircleIcon
+                className="h-5 w-5 cursor-pointer transition-transform duration-150
+              ease-out hover:scale-150 "
+              />
+              <EmojiHappyIcon
+                className="h-5 w-5 cursor-pointer transition-transform duration-150
+              ease-out hover:scale-150 "
+              />
+              <CalendarIcon
+                className="h-5 w-5 cursor-pointer transition-transform duration-150
+              ease-out hover:scale-150 "
+              />
+              <LocationMarkerIcon
+                className="h-5 w-5 cursor-pointer transition-transform duration-150
+              ease-out hover:scale-150 "
+              />
+            </div>
+            <button
+              disabled={!input}
+              className="rounded-full bg-twitter px-5 py-2 font-bold text-white
+              disabled:opacity-40 "
+            >
+              Tweet
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
